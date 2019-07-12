ce98d5ad5147b756d21f33678842829733cf02e4
Outail619 <47230435+Outail619@users.noreply.github.com>
2019-07-12 11:55:03 +0100

    Model "type device" , Schema Only .

---
+++ modules/models/type.server.model.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * Module dependencies
+ */
+var mongoose = require('mongoose'),
+  path = require('path'),
+  Schema = mongoose.Schema,
+
+var TypeDeviceSchema = new Schema({
+  id_type: {
+    type: String,
+    required: 'Fill in a type device ID',
+    lowercase: true,
+    trim: true,
+    index: {
+      unique: true,
+      sparse: true
+    }
+  },
+  name_type: {
+    type: String,
+    required: 'Fill in a type device name'
+  }
+});
