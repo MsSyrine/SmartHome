03d15d704cc828bbd8b30b2f8fccf11b62ca759e
Outail619 <47230435+Outail619@users.noreply.github.com>
2019-07-12 11:53:55 +0100

    Model "Log" , Schema Only .

---
+++ modules/models/log.server.model.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/**
+ * Module dependencies
+ */
+var mongoose = require('mongoose'),
+  path = require('path'),
+  Schema = mongoose.Schema,
+  DeviceModel = require('./device.server.model'),
+  UserModel = require('./user.server.model'),
+
+var LogSchema = new Schema({
+  id_log: {
+    type: String,
+    trim: true,
+    lowercase: true,
+    index: {
+      unique: true,
+      sparse: true
+    }
+  },
+  device: {
+    type: DeviceModel
+  },
+  user: {
+    type: UserModel
+  },
+  log_date: {
+    type: Date,
+    defaults: Date.now
+  },
+  action: {
+    type: String,
+    lowercase: true,
+  },
+  resultat: {
+    type: String,
+    lowercase: true
+  }
+
+});
