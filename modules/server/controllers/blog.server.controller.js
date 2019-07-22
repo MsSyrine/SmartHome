64d991e3f2b0e0c63ce453998bcd9a031035b68b
Outail619 <47230435+Outail619@users.noreply.github.com>
2019-07-22 13:25:19 +0100

    CRUD "blog" , Controllers + routes .

--- modules/server/controllers/blog.server.controller.js
+++ modules/server/controllers/blog.server.controller.js
@@ -13,10 +13,52 @@ exports.AddBlog = function (req, res) {
     brief : req.body.brief,
     description : req.body.description,
     image : req.body.image,
-
       });
       Blog.save((err, doc) => {
         if (!err) { res.send(doc); }
         else { console.log('Error in Blog Save :' + JSON.stringify(err, undefined, 2)); }
       });
-    }
+}
+
+exports.UpdateBlog = function (req, res){
+  if (!ObjectId.isValid(req.params.id))
+    return res.status(400).send(`No record with given id : ${req.params.id}`);
+
+  var Blog = new BlogModel ({
+    title : req.body.title,
+    brief : req.body.brief,
+    description : req.body.description,
+    image : req.body.image,
+  });
+  BlogModel.findByIdAndUpdate(req.params.id, { $set: Blog }, { new: true }, (err, doc) => {
+    if (!err) { res.send(doc); }
+    else { console.log('Error in Blog Update :' + JSON.stringify(err, undefined, 2)); }
+  });
+}
+
+exports.DeleteBlog = function (req, res){
+  if (!ObjectId.isValid(req.params.id))
+    return res.status(400).send(`No record with given id : ${req.params.id}`);
+
+  BlogModel.findByIdAndRemove(req.params.id, (err, doc) => {
+    if (!err) { res.send(doc); }
+    else { console.log('Error in Blog Delete :' + JSON.stringify(err, undefined, 2)); }
+  });
+}
+
+exports.ListBlog = function (req, res){
+  BlogModel.find((err, docs) => {
+    if (!err) { res.send(docs); }
+    else { console.log('Error in Retriving blogs :' + JSON.stringify(err, undefined, 2)); }
+  });
+}
+
+exports.FindBlogByID = function (req, res){
+  if (!ObjectId.isValid(req.params.id))
+        return res.status(400).send(`No record with given id : ${req.params.id}`);
+
+    BlogModel.findById(req.params.id, (err, doc) => {
+        if (!err) { res.send(doc); }
+        else { console.log('Error in Retriving the blog :' + JSON.stringify(err, undefined, 2)); }
+    });
+}
---
+++ modules/server/routes/blog.server.routes.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = function (app) {
+  var BlogController = require('../controllers/blog.server.controller'),
+  users = require('../controllers/users.server.controller');
+
+  app.route('/api/blog/add').post(BlogController.AddBlog);
+  app.route('/api/blog/update'.put(BlogController.UpdateBlog));
+  app.route('/api/blog/delete').delete(BlogController.DeleteBlog);
+  app.route('/api/blog').get(BlogController.ListBlog);
+  app.route('/api/product/:id').get(BlogController.FindBlogByID);
+
+  app.param('userId', users.userByID);
+}
