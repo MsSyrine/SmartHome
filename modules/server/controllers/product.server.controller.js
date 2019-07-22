6943e345974d621aaa954fbf057ecd9b662b456d
Outail619 <47230435+Outail619@users.noreply.github.com>
2019-07-22 12:45:38 +0100

    CRUD "product" , Controller + routes .

---
+++ modules/server/controllers/product.server.controller.js
@@ -0,0 +1,70 @@
+'use strict';
+
+/**
+ * Module dependencies
+ */
+var path = require('path'),
+    mongoose = require('mongoose'),
+    ProductModel = mongoose.model('Product');
+
+exports.AddProduct = function (req, res) {
+  var product = new ProductModel ({
+    id_product : req.body.id_product,
+    product_name : req.body.product_name,
+    image : req.body.image,
+    version : req.body.version,
+    description : req.body.description,
+    stock : req.body.stock,
+    price : req.body.price,
+  });
+  product.save((err, doc) => {
+    if (!err) { res.send(doc); }
+    else { console.log('Error in Product Save :' + JSON.stringify(err, undefined, 2)); }
+  });
+}
+
+exports.UpdateProduct = function (req,res){
+  if (!ObjectId.isValid(req.params.id))
+        return res.status(400).send(`No record with given id : ${req.params.id}`);
+
+    var product = new ProductModel ({
+          id_product : req.body.id_product,
+          product_name : req.body.product_name,
+          image : req.body.image,
+          version : req.body.version,
+          description : req.body.description,
+          stock : req.body.stock,
+          price : req.body.price,
+        });
+    ProductModel.findByIdAndUpdate(req.params.id, { $set: product }, { new: true }, (err, doc) => {
+        if (!err) { res.send(doc); }
+        else { console.log('Error in Product Update :' + JSON.stringify(err, undefined, 2)); }
+    });
+}
+
+exports.DeleteProduct = function (req,res){
+  if (!ObjectId.isValid(req.params.id))
+  return res.status(400).send(`No record with given id : ${req.params.id}`);
+
+  ProductModel.findByIdAndRemove(req.params.id, (err, doc) => {
+    if (!err) { res.send(doc); }
+    else { console.log('Error in Product Delete :' + JSON.stringify(err, undefined, 2)); }
+});
+}
+
+exports.ListProduct = function (req,res){
+  ProductModel.find((err, docs) => {
+    if (!err) { res.send(docs); }
+    else { console.log('Error in Retriving products :' + JSON.stringify(err, undefined, 2)); }
+  });
+}
+
+exports.FindProductByID = function (req,res){
+  if (!ObjectId.isValid(req.params.id))
+        return res.status(400).send(`No record with given id : ${req.params.id}`);
+
+    ProductModel.findById(req.params.id, (err, doc) => {
+        if (!err) { res.send(doc); }
+        else { console.log('Error in Retriving the product :' + JSON.stringify(err, undefined, 2)); }
+    });
+}
---
+++ modules/server/routes/product.server.routes.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = function (app) {
+  var ProductController = require('../controllers/product.server.controller');
+  var users = require('../controllers/users.server.controller');
+
+  app.route('/api/product/add').post(ProductController.AddProduct);
+  app.route('/api/product/update'.put(ProductController.UpdateProduct));
+  app.route('/api/product/delete').delete(ProductController.DeleteProduct);
+  app.route('/api/product').get(ProductController.ListProduct);
+  app.route('api/product/:id').get(ProductController.FindProductByID);
+
+
+  app.param('userId', users.userByID);
+}
