b684acf4a6c2b8e22313ee3f98e60a5c66b3396c
syrine <syrine.benaziza@gmail.com>
2019-07-22 14:22:40 +0100

    modified:   modules/core/server/controllers/core.server.controller.js 	modified:   modules/core/server/views/layout.server.view.html 	created the controller for the devices:   modules/server/controllers/device.server.controller.js 	created the routes for the devices:   modules/server/routes/device.server.routes.js 	deleted:    server/models/device.server.model.js

---
+++ modules/server/routes/device.server.routes.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = function (app) {
+  var ProductController = require('../controllers/device.server.controller');
+  var users = require('../controllers/users.server.controller');
+
+  app.route('/api/device/add').post(deviceController.AddDevice);
+  app.route('/api/device/update'.put(deviceController.UpdateDevice));
+  app.route('/api/device/delete').delete(deviceController.DeleteDevice);
+  app.route('/api/device').get(deviceController.ListDevice);
+  app.route('/api/device/:id').get(deviceController.FindDeviceByID);
+
+
+  app.param('userId', users.userByID);
+}
