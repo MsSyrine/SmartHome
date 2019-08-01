a3b2af0727fb944885ea85d3d3db3790ccdd2e41
syrine <syrine.benaziza@gmail.com>
2019-08-01 17:33:57 +0100

    started working on the home service for quering the DB and linking devices to the user and home

---
+++ modules/home/server/service/home.js
@@ -0,0 +1,86 @@
+/**
+ * Module dependencies
+ */
+var path = require('path'),
+mongoose = require('mongoose'),
+homeModel = mongoose.model('Home'),
+deviceModel = mongoose.model('Device'),
+UserModel = mongoose.model('User');
+
+function extract_data(req){
+    var data = JSON.stringify(req.body);
+    // console.log("********label :*********" + data.home_label);
+    console.log("request data :" + data);
+    var obj = JSON.parse(data);
+    return obj;
+//    var username = obj.owners[0].username;
+}
+// id_home, home_label, owners , priority, startdate, validuntil
+async function updateHomeSettings (req) {
+    var obj = extract_data(req);
+    try {
+        return homeModel.findOneAndUpdate({'id_home': obj.id_home}, {
+            $set: {
+                'home_label': obj.home_label,
+                'owners': [{
+//                    'username': obj.owners[0].username,
+//                    'priority': obj.owners[0].priority,
+                    'startdate': obj.owners[0].startdate,
+                    'validUntil': obj.owners[0].validUntil 
+                }]
+            }
+        }, { runValidators: true }, {new: true})
+    } catch (error) {
+        throw new Error(`Unable to update user with id "${userId}".`)
+    }
+}
+
+// id_home, home_label, owners , priority, startdate, validuntil
+async function addUser (req) {
+    var obj = extract_data(req);
+    try {
+        return homeModel.findOneAndUpdate({'id_home': obj.id_home}, {
+            $set: {
+                'home_label': obj.home_label,
+                'owners': [{
+//                    'username': obj.owners[0].username,
+//                    'priority': obj.owners[0].priority,
+                    'startdate': obj.owners[0].startdate,
+                    'validUntil': obj.owners[0].validUntil
+                }]
+            }
+        }, {new: true})
+    } catch (error) {
+        throw new Error(`Unable to update user with id "${userId}".`)
+    }
+}
+
+async function findHomeByUsername (username) {
+    try {
+        return User.findOne({'username': username})
+    } catch (error) {
+        throw new Error(`Unable to connect to the database.`)
+    }
+}
+
+async function findDevices (home_id) {
+    try {
+//        return deviceModel.populate.findOne({'username': username});
+        return deviceModel.find({ home: home_id })
+        .populate('devices').exec((err, devices) => {
+            console.log("Populated devices " + devices);
+        })
+    } catch (error) {
+        throw new Error(`Unable to connect to the database.`)
+    }
+}
+
+
+// other methods
+
+export default {
+    updateHomeSettings,
+    addUser,
+    findHomeByUsername,
+    findDevices
+}
